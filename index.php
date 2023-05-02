8623354c2cb5ce8400aa62a0d129eed71bf88e22
anafigueiro <114446641+anafigueiro@users.noreply.github.com>
2023-05-02 16:14:57 -0300

    upload_projeto_php_banco

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include "cabecalho.inc.php";
+include "menu.inc.php";
+include "conteudo.inc.php";
+include "rodape.inc.php";
+include "contato.inc.php";
+include "welcome.inc.php";
+
+echo "<a href='paginas/contato.inc.php'> Contato </a>"
+
+?>
---
+++ paginas/ContatoForm.php
@@ -0,0 +1,37 @@
+<?php 
+include './BD.class.php';
+$conn = new BD();
+
+if(!empty($_POST)){
+    $conn->inserir($_POST);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+
+  <title> Document </title>
+
+  <html>
+  <body>
+    <form action= "ContatoForm.php" method="post">
+      <h3> Formulário Contato</h3>
+      <label for=""> Nome: </label>
+      <input type="text" name="nome" id=""> <br>
+
+      <label for=""> Email: </label>
+      <input type="text" name="email" id=""> <br>
+
+      <label for=""> Telefone: </label>
+      <input type="text" name="telefone" id=""> <br>
+
+      <input type="submit" value= "Enviar"> <br>
+    </form>
+  </body>
+  </html>
+    
+  </head>
